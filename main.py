895c8467910c7dc85077122efa72bfeaa0816982
widawek <kolczywiewicz@gmail.com>
2024-01-09 16:11:19 +0100

    Update movies list, and update hompage.html

--- main.py
+++ main.py
@@ -5,7 +5,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def homepage():
-    movies = [_ for _ in range(10)]
+    movies = [
+        ("Matrix", "https://fwcdn.pl/fpo/06/28/628/7685907.6.jpg",
+         "https://www.filmweb.pl/film/Matrix-1999-628"),
+        ("The Godfather", "https://fwcdn.pl/fpo/10/89/1089/7196615.6.jpg",
+         "https://www.filmweb.pl/film/Ojciec+chrzestny-1972-1089"),
+        ("Forest Gump", "https://fwcdn.pl/fpo/09/98/998/8021615.6.jpg",
+         "https://www.filmweb.pl/film/Forrest+Gump-1994-998"),
+        ("Attack of the Killer Donuts", "https://fwcdn.pl/fpo/79/41/747941/7774868.6.jpg",
+         "https://www.filmweb.pl/film/Atak+krwio%C5%BCerczych+donat%C3%B3w-2016-747941")
+              ]
     return render_template("homepage.html", movies=movies)
 
 
